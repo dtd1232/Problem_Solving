63ca526b19e05913866ff7d009a2c1a914019d32
Seungyong Lee (Noah) <dtd12321@gmail.com>
2024-04-03 20:58:14 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+long long power(long long b) {
+	if (b == 0) return 1;
+	if (b == 1) return a % c;
+	
+	if (b % 2 == 0) return power(b / 2) % c * power(b / 2) % c % c;
+	return power(b / 2) % c * power(b / 2) % c % c * a % c;
+}
+
+int main(void) {
+	cin >> a >> b >> c; 
+	cout << power(b);
+
+	return 0;
+}
