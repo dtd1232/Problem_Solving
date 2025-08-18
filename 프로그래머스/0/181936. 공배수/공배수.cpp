36118d156b2f7afc743991548ef07189c7ed149b
Seungyong Lee <dtd12321@gmail.com>
2025-08-18 21:01:31 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+    
+    if(number % n != 0) return 0;
+    if(number % m != 0) return 0;
+
+    return 1;
+}
